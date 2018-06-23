fa10b2911d63fe5f804d8551a7ada7bdeaa5ffe4
Sanjay Thakur <sanjay@live.com>
2018-06-24 02:01:35 +0530

    ui fixes

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,12 +2,15 @@
 
 @section('content')
 <div class="container">
+    <div class="block-header">
+                <h2>&nbsp;</h2>
+            </div>
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 col-md-offset-2">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="header"><h2>{{ __('RESET PASSWORD') }}</h2></div>
 
-                <div class="card-body">
+                <div class="body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
@@ -20,9 +23,10 @@
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
+                            <div class="col-md-12">
+                                <div class="form-line">
+                                <input id="email" type="email" placeholder="Enter Your Email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+</div>
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -33,7 +37,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary btn-lg">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
